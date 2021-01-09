4110b0dc98c8cdf67da25788c565e294635abdae
melon <meloncha0205@gmail.com>
2021-01-09 15:08:17 +0900

    add HelloController and Test codes

---
+++ src/test/java/com/ssafy/book/springboot/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,24 @@
+package com.ssafy.book.springboot.web.dto;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloResponseDtoTest {
+
+    @Test
+    public void 롬복_기능_테스트() {
+        // given
+        String name = "test";
+        int amount = 1000;
+
+        // when
+        HelloResponseDto helloResponseDto = new HelloResponseDto(name, amount);
+
+        // then
+        assertThat(helloResponseDto.getName()).isEqualTo(name);
+        assertThat(helloResponseDto.getAmount()).isEqualTo(amount);
+    }
+
+}
